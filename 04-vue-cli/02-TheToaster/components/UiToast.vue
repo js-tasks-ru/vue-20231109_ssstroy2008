89378003942b690856575204a59ec0d41ab8412f
V1kaNikolaeva <114816050+V1kaNikolaeva@users.noreply.github.com>
2023-12-09 23:09:18 +0300

    commit

---
+++ 04-vue-cli/02-TheToaster/components/UiToast.vue
@@ -0,0 +1,16 @@
+<template>
+  <span>{{ toast.message }}</span>
+</template>
+
+<script>
+export default {
+  name: 'UiToast',
+  
+  props: {
+    toast: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
